e2b46f5f48ef81560c0ba4f0dba4053ad14b4c2f
Xinchang Cathy Li <xinchang.cathy.li@gmail.com>
2026-08-02 20:02:34 -0700

    Correct publication author names

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -12,7 +12,7 @@ export type Publication = {
 export const publications: Publication[] = [
   {
     title: "Divergent humidity-driven growth in urban air-conditioning energy demand under climate change",
-    authors: "Xinchang Li, Lei Zhao, Zhiwen Luo, Keith Oleson, Yichi Cheng, Xiangyu Xie, Alvin Varquez, Masayuki Sekiya",
+    authors: "Xinchang ‘Cathy’ Li, Lei Zhao, Zhiwen Luo, Keith Oleson, Yifan Cheng, Xiaoxiong Xie, Alvin C. G. Varquez, Mitsuna Sekiya",
     venue: "Nature Cities",
     year: 2026,
     doi: "https://doi.org/10.1038/s44284-026-00474-4",
@@ -21,7 +21,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Divergent surface urban heat island responses to background aerosol mitigation",
-    authors: "Shasha Liu, Yuyu Zhou, Lei Zhao, Gerald Mills, Xinchang Li, Han Zhang, Karen Seto, Gensuo Jia",
+    authors: "Shu Liu, Yuyu Zhou, Lei Zhao, Gerald Mills, Xinchang “Cathy” Li, Hongsheng Zhang, Karen C. Seto, Peng Gong",
     venue: "Nature Cities",
     year: 2026,
     doi: "https://doi.org/10.1038/s44284-026-00463-7",
@@ -29,7 +29,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Mitigating urban climate–energy feedback with citywide building-integrated photovoltaics implementation",
-    authors: "Lin Chen, Shuyang Zhang, Xinchang Li, Ting Fong May Chui, Junjing Yang, Wei Wang, Tianzhen Hong, Zhaosong Wang",
+    authors: "Liutao Chen, Shihong Zhang, Xinchang “Cathy” Li, Ting Fong May Chui, Jiachuan Yang, Wei Wang, Tianzhen Hong, Zhe Wang",
     venue: "Nexus",
     year: 2026,
     doi: "https://doi.org/10.1016/j.ynexs.2025.100114",
@@ -37,7 +37,7 @@ export const publications: Publication[] = [
   },
   {
     title: "U-Surf: A global 1 km spatially continuous urban surface property dataset for kilometer-scale urban-resolving Earth system modeling",
-    authors: "Yichi Cheng, Lei Zhao, Tirthankar Chakraborty, Keith Oleson, Matthias Demuzere, Xiaoping Liu, Yuan Che, Weilin Liao, Yuyu Zhou, Xinchang Li",
+    authors: "Yifan Cheng, Lei Zhao, TC Chakraborty, Keith Oleson, Matthias Demuzere, Xiaoping Liu, Yangzi Che, Weilin Liao, Yuyu Zhou, Xinchang “Cathy” Li",
     venue: "Earth System Science Data",
     year: 2025,
     doi: "https://doi.org/10.5194/essd-17-2147-2025",
@@ -45,7 +45,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Enhancing urban thermal environment and energy sustainability with temperature-adaptive radiative roofs",
-    authors: "Kai Zhang, Lei Zhao, Keith Oleson, Xinchang Li, Xuhui Lee",
+    authors: "Keer Zhang, Lei Zhao, Keith Oleson, Xinchang “Cathy” Li, Xuhui Lee",
     venue: "Earth’s Future",
     year: 2025,
     doi: "https://doi.org/10.1029/2024EF005246",
@@ -53,7 +53,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Elevated urban energy risks due to climate-driven biophysical feedbacks",
-    authors: "Xinchang Li, Lei Zhao, Yi Qin, Keith Oleson, Yunfeng Zhang",
+    authors: "Xinchang ‘Cathy’ Li, Lei Zhao, Yue Qin, Keith Oleson, Yiwen Zhang",
     venue: "Nature Climate Change",
     year: 2024,
     doi: "https://doi.org/10.1038/s41558-024-02108-w",
@@ -63,7 +63,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Enhancing urban climate–energy modeling in the Community Earth System Model through explicit representation of urban air-conditioning adoption",
-    authors: "Xinchang Li, Lei Zhao, Keith Oleson, Yuyu Zhou, Yi Qin, Kai Zhang, Bin Fang",
+    authors: "Xinchang “Cathy” Li, Lei Zhao, Keith Oleson, Yuyu Zhou, Yue Qin, Keer Zhang, Bowen Fang",
     venue: "Journal of Advances in Modeling Earth Systems",
     year: 2024,
     doi: "https://doi.org/10.1029/2023MS004107",
@@ -73,7 +73,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Analysis of radiation-induced cooling and growth of mist and cloud droplets",
-    authors: "M. Quinn Brewster, Xinchang Li",
+    authors: "M. Brewster, X. Li",
     venue: "International Journal of Heat and Mass Transfer",
     year: 2020,
     doi: "https://doi.org/10.1016/j.ijheatmasstransfer.2020.120674",
@@ -81,7 +81,7 @@ export const publications: Publication[] = [
   },
   {
     title: "Radiation-induced condensational growth and cooling of cloud-sized mist droplets",
-    authors: "M. Quinn Brewster, Xinchang Li, K. Roman, E. McNichols, Mark Rood",
+    authors: "M. Q. Brewster, X. Li, K. K. Roman, E. O. McNichols, M. J. Rood",
     venue: "Journal of the Atmospheric Sciences",
     year: 2020,
     doi: "https://doi.org/10.1175/JAS-D-19-0288.1",
